027f7de90f22cea618f0db0a4d9387975b6c7c47
Jonny Buchanan <jonathan.buchanan@gmail.com>
2017-05-29 07:26:15 +1000

    Create a local copy of Bootstrap's variable config and use it in our app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import './bootstrap.scss'
+
 import React from 'react'
 
 export default class App extends React.Component {
   render() {
     return <div>
-      <h2>Welcome to React</h2>
+      <div class="container">
+        <h2>Welcome to React</h2>
+        <button class="btn btn-primary">Button</button>
+      </div>
     </div>
   }
 }
