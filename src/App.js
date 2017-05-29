1c7cea741c8ce5ab30956c40c53ec467359d4560
Jonny Buchanan <jonathan.buchanan@gmail.com>
2017-05-29 12:36:35 +1000

    Pull in react-fa and use it for the spinner in a LoadingButton component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './bootstrap.scss'
 import React from 'react'
 
 import Blog from './Blog'
+import LoadingButton from './LoadingButton'
 
 export default class App extends React.Component {
   render() {
     return <div>
       <div class="container">
         <h2>Welcome to React</h2>
-        <button class="btn btn-primary">Button</button>
+        <div class="btn-group" role="group" aria-label="Example buttons">
+          <LoadingButton>Not Loading Button</LoadingButton>
+          <LoadingButton loading>Loading Button</LoadingButton>
+          <LoadingButton bsStyle="info">Jelly Button</LoadingButton>
+        </div>
       </div>
       <hr/>
       <Blog/>
---
+++ src/LoadingButton.js
@@ -0,0 +1,28 @@
+import t from 'prop-types'
+import React from 'react'
+import Icon from 'react-fa'
+
+export default class LoadingButton extends React.Component {
+  static propTypes = {
+    loading: t.bool.isRequired,
+    bsStyle: t.string,
+    type: t.string,
+  }
+
+  static defaultProps = {
+    bsStyle: 'primary',
+    type: 'button',
+  }
+
+  render() {
+    let {bsStyle, children, className, disabled, loading, ...props} = this.props
+    let classes = `btn btn-${bsStyle}`
+    if (className) {
+      classes += ` ${className}`
+    }
+    disabled = disabled || loading
+    return <button class={classes} disabled={disabled} {...props}>
+      {loading && <Icon name="spinner" spin/>} {children}
+    </button>
+  }
+}
